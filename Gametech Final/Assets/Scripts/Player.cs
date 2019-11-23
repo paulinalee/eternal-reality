1e78ed5174f6bf73735b65be669db212b1e12b15
Paulina Lee <0618py@gmail.com>
2019-11-22 20:34:17 -0600

    fixing jankass movement

--- Gametech Final/Assets/Scripts/Player.cs
+++ Gametech Final/Assets/Scripts/Player.cs
@@ -8,7 +8,6 @@ public class Player : MonoBehaviour {
 	public float speed = 5.0f;
 	public float gravity = 9.8f;
 	private Vector3 direction = Vector3.zero;
-
 	// Use this for initialization
 	void Start () {
 		characterController = GetComponent<CharacterController>();
@@ -16,18 +15,22 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (characterController.isGrounded)
-        {
+        if (characterController.isGrounded){
             direction = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
             direction *= speed;
-
-            if (Input.GetButton("Jump"))
-            {
+            if (Input.GetButton ("Jump")) {
                 direction.y = speed;
             }
+        } else {
+            direction = new Vector3(Input.GetAxis("Horizontal"), direction.y,
+            Input.GetAxis("Vertical"));
+            direction.x *= speed;
+            direction.z *= speed;
         }
-		
+
         direction.y -= gravity * Time.deltaTime;
+        direction = transform.TransformDirection(direction);
         characterController.Move(direction * Time.deltaTime);
+
 	}
 }
